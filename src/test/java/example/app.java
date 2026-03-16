cce381c4a3b928c96fade4cf3ebfa69c9ed5a5c4
Gangurde Vinay Shivdas <vinaygangurde2101@gmail.com>
2026-03-16 15:17:18 +0530

    Create app.java

---
+++ src/test/java/example/app.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Jenkins CI/CD Pipeline!");
+        System.out.println("Build executed successfully.");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum of numbers: " + sum);
+    }
+}
