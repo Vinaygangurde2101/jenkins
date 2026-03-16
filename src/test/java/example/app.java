25949ce37a5fd8d2b744af51eee5f26699508867
Gangurde Vinay Shivdas <vinaygangurde2101@gmail.com>
2026-03-16 15:21:47 +0530

    Update app.java

--- src/test/java/example/app.java
+++ src/test/java/example/app.java
@@ -1,15 +1,12 @@
+package example;
+
 public class App {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello from Jenkins CI/CD Pipeline!");
-        System.out.println("Build executed successfully.");
-
+        System.out.println("Hello Jenkins Pipeline");
         int a = 10;
         int b = 20;
-
-        int sum = a + b;
-
-        System.out.println("Sum of numbers: " + sum);
+        System.out.println("Sum = " + (a + b));
     }
+
 }
